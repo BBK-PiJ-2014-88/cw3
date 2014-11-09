b5ac0eea5828c412dbb56136cb91223ae6c697b1
Sergio <rurounisergii@hotmail.com>
2014-11-09 17:54:57 +0000

    trying to figure out how to get ReturnObject to hold any dkind of data type. Experimenting with using data type Object

--- ReturnObjectImpl.java
+++ ReturnObjectImpl.java
@@ -1,4 +1,12 @@
 public class ReturnObjectImpl implements ReturnObject{
+
+	Object Value;
+
+	public void setValue(Object valueToAdd){
+
+		this.Value = new ValueToAdd.getClass();
+	}
+
 	public boolean hasError(){
 	}
 	public ErrorMessage getError(){
