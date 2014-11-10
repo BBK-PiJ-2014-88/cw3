57bd82c77ca87c0074b2fd1f44bba75558171c72
Sergio <rurounisergii@hotmail.com>
2014-11-10 20:29:02 +0000

    completed method for adding element into arraylist at particular index

--- ArrayList.java
+++ ArrayList.java
@@ -34,7 +34,7 @@ public class ArrayList implements List{
 	}
 	public ReturnObject add(Object item){
 		if (item == null){
-			ReturnObjectImpl errorObject = new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+			ReturnObject errorObject = new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
 			return errorObject;
 		}
 		this.isNearlyFull();
@@ -43,10 +43,34 @@ public class ArrayList implements List{
 
 	}
 	public ReturnObject add(int index, Object item){
+		if (index < 0 || index >= theActualList.length){
+			ReturnObject errorObject = new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+			return errorObject;
+		}
+		else if (item == null){
+			ReturnObject errorObject = new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+			return errorObject;
+		}
+		else if(this.theActualList[index].equals(new String("Empty"))){
+			this.theActualList[index] = item;
+			return (new ReturnObjectImpl(null)); //returning empty ReturnObject as operation was successful
+		}
+		else{ //this means there is already an object in the list at that index so will need to rearrange objects to make room
+			this.isNearlyFull(); //check if list is nearly full first before moving objects one place to the right
+			Object temp = theActualList[index];
+			Object temp2;
+			for (int x = index; x < theActualList.length; x++){
+				temp2 = theActualList[x + 1];
+				this.theActualList[index + 1] = temp;
+				temp = temp2;
+			}
+			this.theActualList[index] = item;
+			return (new ReturnObjectImpl(null));
+		}
 	}
 
 	public void isNearlyFull(){
-		int lengthMinusOne = this.theActualList.length -1;
+		int lengthMinusOne = this.theActualList.length -2;
 		String str = new String("Empty");
 		if (!((this.theActualList[lengthMinusOne]).equals(str))){  //ArrayList nearly full
 			Object[] temp = new Object[(this.theActualList.length) *2];
