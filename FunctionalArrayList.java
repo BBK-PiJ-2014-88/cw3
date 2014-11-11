875e8baf70e64237574ff3b53e67e46577a10df2
Sergio <rurounisergii@hotmail.com>
2014-11-11 19:47:08 +0000

    added head method

--- FunctionalArrayList.java
+++ FunctionalArrayList.java
@@ -1,6 +1,15 @@
-public class FunctionalArrayList implements FunctionalList extends ArrayList{
+public class FunctionalArrayList extends ArrayList implements FunctionalList{
+
 	public ReturnObject head(){
+		if (this.isEmpty() == true){
+			ReturnObject errorObject2 = new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+			return errorObject2;
+		}
+		else{
+			return new ReturnObjectImpl(theActualList[0]);
+		}
 	}
+
 	public FunctionalList rest(){
 	}
 
