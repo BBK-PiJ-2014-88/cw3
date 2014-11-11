5e15a5948a4287a06aec50e85658f0073341198a
Sergio <rurounisergii@hotmail.com>
2014-11-11 20:40:32 +0000

    FunctionalLinkedList is not compiling either. Having problem sorting out inheritance

--- FunctionalLinkedList.java
+++ FunctionalLinkedList.java
@@ -1,2 +1,25 @@
-public class FunctionalLinkedList implements FunctionalList extends LinkedList{
+public class FunctionalLinkedList extends LinkedList implements FunctionalList {
+	public ReturnObject head(){
+		if (this.isEmpty() == true){
+			ReturnObject errorObject2 = new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+			return errorObject2;
+		}
+		else{
+			return (new ReturnObjectImpl(firstNodeInList.getElement()));
+		}
+	}
+	public FunctionalList rest(){
+		FunctionalList newFunctionalList = new LinkedList();
+		if (this.isEmpty() == true){
+			return newFunctionalList;
+		}
+		else{
+			for (int x = 1; x < this.size(); x++){
+				newFunctionalList.add((this.get(x)).getReturnValue());
+				return newFunctionalList;
+			}
+		}
+
+
+	}
 }
