49df597ae07134b874633f47d5c89c6b246bcd67
Sergio <rurounisergii@hotmail.com>
2014-11-12 14:49:31 +0000

    also got rid of .this in FunctionalLinkedList and made minor correction, changing name of data type from LinkedList to FunctionalLinkedList

--- FunctionalLinkedList.java
+++ FunctionalLinkedList.java
@@ -1,6 +1,6 @@
-public class FunctionalLinkedList extends LinkedList implements FunctionalList {
+public class FunctionalLinkedList extends LinkedList implements FunctionalList{
 	public ReturnObject head(){
-		if (this.isEmpty() == true){
+		if (isEmpty() == true){
 			ReturnObject errorObject2 = new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
 			return errorObject2;
 		}
@@ -9,17 +9,15 @@ public class FunctionalLinkedList extends LinkedList implements FunctionalList {
 		}
 	}
 	public FunctionalList rest(){
-		FunctionalList newFunctionalList = new LinkedList();
-		if (this.isEmpty() == true){
+		FunctionalLinkedList newFunctionalList = new FunctionalLinkedList();
+		if (isEmpty() == true){
 			return newFunctionalList;
 		}
 		else{
 			for (int x = 1; x < this.size(); x++){
 				newFunctionalList.add((this.get(x)).getReturnValue());
-				return newFunctionalList;
 			}
+			return newFunctionalList;
 		}
-
-
 	}
 }
