1af8247b69c1cd5fb54ef12ab2b26b20f97ea2ab
Sergio <rurounisergii@hotmail.com>
2014-11-12 21:15:45 +0000

    added Stack methods which work by calling on internalList methods

--- StackImpl.java
+++ StackImpl.java
@@ -1,3 +1,13 @@
 public class StackImpl extends AbstractStack implements Stack{
+	@Override
+	public boolean isEmpty(){
+		return internalList.getEmpty();
+	}
+	public int size(){
+		return internalList.size();
+	}
+	public void push(){
+
+	}
 
 }
