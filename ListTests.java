9dcc380d00ecd75efdd7444089679709591ab1cc
Sergio <rurounisergii@hotmail.com>
2014-11-12 15:13:49 +0000

    added more tests for isEmpty method

--- ListTests.java
+++ ListTests.java
@@ -5,15 +5,39 @@ public class ListTests{
 	private FunctionalArrayList arrayListTester;
 	private FunctionalLinkedList linkedListTester;
 
+	//Testing isEmpty() with an empty list
 	@Before
 	public void buildUp(){
 		arrayListTester = new FunctionalArrayList();
 		linkedListTester = new FunctionalLinkedList();
 	}
 	@Test
-	public void testIsEmpty(){
+	public void testIsEmptyArrayOnEmptyList(){
 		boolean expected = true;
 		boolean output = arrayListTester.isEmpty();
 		assertEquals(output, expected);
 	}
+	@Test
+	public void testIsEmptyLinkedOnEmptyList(){
+		boolean expected = true;
+		boolean output = linkedListTester.isEmpty();
+		assertEquals(output, expected);
+	}
+
+	//Testing isEmpty() with a non-empty List
+	@Test
+	public void testIsEmptyArrayOnNonEmptyList(){
+		arrayListTester.add("hello");
+		boolean expected = false;
+		boolean output = arrayListTester.isEmpty();
+		assertEquals(output, expected);
+	}
+	@Test
+	public void testIsEmptyLinkedOnNonEmptyList(){
+		linkedListTester.add("Goodbye");
+		boolean expected = false;
+		boolean output = linkedListTester.isEmpty();
+		assertEquals(output, expected);
+	}
+
 }
