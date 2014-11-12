6ccfa1cccd0fb44e3852ebb736240e706a34bcc9
Sergio <rurounisergii@hotmail.com>
2014-11-12 18:56:54 +0000

    added List methods to SampleableListImpl

--- SampleableListImpl.java
+++ SampleableListImpl.java
@@ -3,4 +3,22 @@ public class SampleableListImpl implements SampleableList{
 	public SampleableListImpl(){
 		listMethodsContainer = new FunctionalLinkedList();
 	}
+	public boolean isEmpty(){
+		return listMethodsContainer.isEmpty();
+	}
+	public int size(){
+		return listMethodsContainer.size();
+	}
+	public ReturnObject get(int index){
+		return listMethodsContainer.get(index);
+	}
+	public ReturnObject remove(int index){
+		return listMethodsContainer.remove(index);
+	}
+	public ReturnObject add(int index, Object item){
+		return listMethodsContainer.add(index, item);
+	}
+	public ReturnObject add(Object item){
+		return listMethodsContainer.add(item);
+	}
 }
