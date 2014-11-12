6e24147ab9ba617f1d60e65d96fd50dcedee859a
Sergio <rurounisergii@hotmail.com>
2014-11-12 17:17:30 +0000

    added some more tests, discovered that my FunctionalArrayList rest() method is not working

--- ArrayListTest.java
+++ ArrayListTest.java
@@ -1,4 +1,7 @@
 public class ArrayListTest{
+	//This file is called ArrayListTest but i use it as a test file for all my lists for certain things
+	//For example printing out the lists and looking at them to make sure the other elements haven't been changed
+	//after an add or remove method
 	public static void main(String[] args){
 		ArrayListTest tester = new ArrayListTest();
 		tester.launch();
@@ -60,5 +63,31 @@ public class ArrayListTest{
 			System.out.println(linkedListTester);
 			linkedListTester.add(4, "four");
 			System.out.println(linkedListTester);
+
+			System.out.println("Functional List tests start here");
+			FunctionalLinkedList functionalLinkedTester = new FunctionalLinkedList();
+			FunctionalArrayList functionalArrayTester = new FunctionalArrayList();
+			System.out.println(functionalLinkedTester.head());
+			functionalLinkedTester.add("1");
+			functionalLinkedTester.add("2");
+			functionalLinkedTester.add("3");
+			functionalLinkedTester.add("4");
+			functionalLinkedTester.add("5");
+			functionalLinkedTester.add("6");
+			System.out.println(functionalLinkedTester.head());
+			System.out.println(functionalLinkedTester.rest());
+
+			System.out.println(functionalArrayTester.head());
+			functionalArrayTester.add("1");
+			functionalArrayTester.add("2");
+			functionalArrayTester.add("3");
+			functionalArrayTester.add("4");
+			functionalArrayTester.add("5");
+			functionalArrayTester.add("6");
+			functionalArrayTester.add("7");
+			System.out.println(functionalArrayTester);
+			System.out.println(functionalArrayTester.head());
+			System.out.println(functionalArrayTester.rest());
+
 	}
 }
