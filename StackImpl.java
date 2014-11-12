6241dfc723f045dc87775b85f54119e952663145
Sergio <rurounisergii@hotmail.com>
2014-11-12 21:17:10 +0000

    fixed constructor i think

--- StackImpl.java
+++ StackImpl.java
@@ -1,13 +1,21 @@
 public class StackImpl extends AbstractStack implements Stack{
-	@Override
+	public StackImpl(List list){
+		super(list);
+	}
 	public boolean isEmpty(){
 		return internalList.getEmpty();
 	}
 	public int size(){
 		return internalList.size();
 	}
-	public void push(){
-
+	public void push(Object item){
+		return internalList.add(0, item);
+	}
+	public ReturnObject top(){
+		return internalList.get(0);
+	}
+	public ReturnObject pop(){
+		return internalList.remove(0);
 	}
 
 }
