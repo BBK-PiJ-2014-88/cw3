55aa9b51a06f9916d28740135a6bf7a22d4728c3
Sergio <rurounisergii@hotmail.com>
2014-11-13 11:24:03 +0000

    Added stack methods to ImprovedStack as ImprovedStack interface extends Stack so should contain its methods. I implemented this by adding an object of Stackimpl into the class and calling its methods for each method

--- ImprovedStackImpl.java
+++ ImprovedStackImpl.java
@@ -1,4 +1,21 @@
 public class ImprovedStackImpl implements ImprovedStack{
 	FunctionalLinkedList stackInternalList = new FunctionalLinkedList();
 	StackImpl internalStack = new StackImpl(stackInternalList);
+
+	public boolean isEmpty(){
+		return internalStack.isEmpty();
+	}
+	public int size(){
+		return internalStack.size();
+	}
+	public void push(Object item){
+		internalStack.push(item);
+	}
+	public ReturnObject top(){
+		return internalStack.top();
+	}
+	public ReturnObject pop(){
+		return internalStack.pop();
+	}
+
 }
