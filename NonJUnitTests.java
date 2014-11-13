e4f1d75cbb2bdabbe905df00bac24ef275e3049b
Sergio <rurounisergii@hotmail.com>
2014-11-13 17:22:20 +0000

    Non JUnit Tests will go here

--- NonJUnitTests.java
+++ NonJUnitTests.java
@@ -1,2 +1,12 @@
 public class NonJUnitTests{
+	public static void main(String[] args){
+		NonJUnitTests tests = new NonJUnitTests();
+		tests.launch();
+	}
+	public void launch(){
+		ImprovedStackImpl improvedStackTester = new ImprovedStackImpl();
+		System.out.println("Testing Improved Stack");
+		System.out.println("Testing isEmpty on empty stack. Expect true");
+		System.out.println(improvedStackTester.isEmpty());
+	}
 }
